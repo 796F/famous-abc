92cb5c00d6e26c1670c31ddde423ff6ce9870702
zpalacios20 <zacharyp@stanford.edu>
2014-08-19 16:04:48 -0700

    bug fix

--- client/templates/code.js
+++ client/templates/code.js
@@ -54,12 +54,10 @@ if (Meteor.isClient) {
 makeLinks = function (code) {
   //given a code object, use its tokenArrays field to tag its content field.
   var tokenArr = code.tokenArray;
-  var content = code.content;
   for(var i = 0; i < tokenArr.length; i++) {
     var token = tokenArr[i];
     var re = new RegExp(token, "g");
-    var newContent = content.replace(re, "<span class='link' data-link='" + token + "'>" + token + "</span>");
-    code.content = newContent;
+    code.content = content.replace(re, "<span class='link' data-link='" + token + "'>" + token + "</span>");
   }
   return code;
 }
